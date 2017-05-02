a3f84a358f0fa6e742b8165ab177463a54c10e95
YuqingLethe <yyq2983026@hotmail.com>
2017-05-01 20:40:37 -0400

    Set the mapbox with L.tilelayer and some markers

--- TinyHouse/src/mapcode.js
+++ TinyHouse/src/mapcode.js
@@ -6,32 +6,23 @@
 
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+L.mapbox.tileLayer('mapbox.streets').addTo(mymap);
 
-//L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-//    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-//    maxZoom: 18,
-//    id: 'eatbanli',
-//    accessToken: 'your.mapbox.public.access.token'
-//}).addTo(mymap);
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
+        <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
+        Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets'
+    accessToken: 'pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm9kMmNrMDEzajMzbzV4dzlvZDJ4YiJ9.Vd_nQMEfIVtqRytVSCDQOg'
+}).addTo(mymap);
 
-//L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//	maxZoom: 19,
-//	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-//}).addTo(mymap);
+mapboxgl.accessToken = 'pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm9kMmNrMDEzajMzbzV4dzlvZDJ4YiJ9.Vd_nQMEfIVtqRytVSCDQOg';
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v9'
+    });
 
-
-
-
-
-	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-		maxZoom: 18,
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-			'mkk<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		id: 'mapbox.streets'
-	}).addTo(mymap);
 
 
 
@@ -39,4 +30,6 @@ var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 //<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
 //  type="text/javascript"></script>
 //AIzaSyDSVQt05mIwLeN_oZlsBklyz2z8mR-GbYs
+
+
 
