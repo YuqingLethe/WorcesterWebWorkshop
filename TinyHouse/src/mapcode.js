86d7668a3dae2937c8911b42dc7555033ef69741
Yuqing <yyq2983026@hotmail.com>
2017-05-04 22:08:27 -0400

    javascript file added

--- TinyHouse/src/mapcode.js
+++ TinyHouse/src/mapcode.js
@@ -4,32 +4,36 @@
  */
 
 
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.mapbox.tileLayer('mapbox.streets').addTo(mymap);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,
-        <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,
-        Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets'
-    accessToken: 'pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm9kMmNrMDEzajMzbzV4dzlvZDJ4YiJ9.Vd_nQMEfIVtqRytVSCDQOg'
-}).addTo(mymap);
-
+/*************** MapBox Only************************/
 mapboxgl.accessToken = 'pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm9kMmNrMDEzajMzbzV4dzlvZDJ4YiJ9.Vd_nQMEfIVtqRytVSCDQOg';
-    var map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v9'
-    });
+var mapBoxMap = new mapboxgl.Map({
+ container: 'mapboxOnly',
+ style: 'mapbox://styles/mapbox/streets-v9'
+});
 
 
+/*************** Leaflet + MapBox ************************/
+//var mymap = L.map('mapid').setView([51.505, -0.09], 13, 13);
+var mymap = L.map('mapid').setView([51.505, 100.09], 13);
+var layer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm85Zm8wMDExbDMyc2Nyamg1Z3JmOSJ9.hHJd89gkLS-oC6QSs41ojA' ).addTo(mymap);
+mymap.setMaxBounds([
+    [5.499550, -167.276413], //Southwest
+     [83.162102, -52.233040]  //Northeast
+]);
 
-
-
-//<script async defer src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap"
-//  type="text/javascript"></script>
-//AIzaSyDSVQt05mIwLeN_oZlsBklyz2z8mR-GbYs
-
-
-
+/*************** Markers ************************/
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var circle = L.circle([51.508, -0.11], {
+                        color: 'red',
+                        fillColor: '#f03',
+                        fillOpacity: 0.5,
+                        radius: 500
+                    }).addTo(mymap);
+var polygon = L.polygon([
+                        [51.509, -0.08],
+                        [51.503, -0.06],
+                        [51.51, -0.047]
+                        ]).addTo(mymap);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
