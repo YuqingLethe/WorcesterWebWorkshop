9443a3711ff4350d0540890589b2fa77dc91408e
Yuqing <yyq2983026@hotmail.com>
2017-05-05 14:07:38 -0400

    map cords changed: lat and lng

--- TinyHouse/src/mapcode.js
+++ TinyHouse/src/mapcode.js
@@ -14,26 +14,26 @@ var mapBoxMap = new mapboxgl.Map({
 
 /*************** Leaflet + MapBox ************************/
 //var mymap = L.map('mapid').setView([51.505, -0.09], 13, 13);
-var mymap = L.map('mapid').setView([51.505, 100.09], 13);
+var mymap = L.map('mapid').setView([42.2581, -71.8144], 13);
 var layer = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZWF0YmFubGkiLCJhIjoiY2oyNm85Zm8wMDExbDMyc2Nyamg1Z3JmOSJ9.hHJd89gkLS-oC6QSs41ojA' ).addTo(mymap);
-mymap.setMaxBounds([
-    [5.499550, -167.276413], //Southwest
-     [83.162102, -52.233040]  //Northeast
-]);
+//mymap.setMaxBounds([
+//    [5.499550, -167.276413], //Southwest
+//     [83.162102, -52.233040]  //Northeast
+//]);
 
 /*************** Markers ************************/
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-var circle = L.circle([51.508, -0.11], {
+var marker = L.marker([42.2481, -71.8155]).addTo(mymap);
+var circle = L.circle([42.2580, -71.8269], {
                         color: 'red',
                         fillColor: '#f03',
                         fillOpacity: 0.5,
-                        radius: 500
+                        radius: 1000
                     }).addTo(mymap);
 var polygon = L.polygon([
-                        [51.509, -0.08],
-                        [51.503, -0.06],
-                        [51.51, -0.047]
+                        [42.2581, -71.8044],
+                        [42.2381, -71.8144],
+                        [42.2781, -71.8244]
                         ]).addTo(mymap);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-circle.bindPopup("I am a circle.");
-polygon.bindPopup("I am a polygon.");
+marker.bindPopup("<b>Good Spot!/b><br>I am a popup.").openPopup();
+circle.bindPopup("Restriction needed.");
+polygon.bindPopup("Ideal place to build.");
